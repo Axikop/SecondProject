3055ee6b5ad1ff2a350791bec82701406ce60823
Axikop <117171938+Axikop@users.noreply.github.com>
2023-10-12 11:25:42 -0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+admin.site.site_header = "Your Admin Site Header"
+admin.site.site_title = "Your Admin Site Title"
+
+
+admin.site.index_title = "Welcome to Your Admin Dashboard"
